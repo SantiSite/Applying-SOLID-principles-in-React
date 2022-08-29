564ed21a4a32eaff11e7430c8e714a567b0347bb
Santiago Restrepo <santiagorv246@gmail.com>
2022-08-29 00:39:40 -0500

    fix

--- src/LiskovSubstitutionPrinciple/index.tsx
+++ src/LiskovSubstitutionPrinciple/index.tsx
@@ -1,12 +1,25 @@
+import { ReactElement } from "react";
+
 // Liskov Substitution Principle: Se puede definir como de relación entre objetos donde
 // "El objeto de subtipo debe ser sustituible con el objeto de supertipo".
 
+type ButtonType = {
+    children: string;
+    size: string;
+    color?: string;
+}
+
+type RedButtonType = {
+    children: string;
+    size: string;
+}
+
 const LiskovSubstitutionPrinciple = () => {
-    const Button = ({children, size, color = 'blue'}) => {
+    const Button = ({children, size, color = 'blue'}: ButtonType) => {
         return <button style={{color, fontSize: size === 'xl' ? '32px' : '16px'}}>{children}</button>
     };
 
-    const RedButton = ({children, size}) => <Button color="red" size={size}>{children}</Button>;
+    const RedButton = ({children, size}: RedButtonType) => <Button color="red" size={size}>{children}</Button>;
 
 
     // Si cambiamos el RedButton por su supertipo Button, no afecta a la aplicación.
